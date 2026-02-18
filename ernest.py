37ab9ae8928245b217fa0871c36c1073442b19f4
Ernest076 <matataernest66@gmail.com>
2026-02-18 21:43:24 +0300

    ernest.py

---
+++ ernest.py
@@ -0,0 +1,17 @@
+# Name : Ernest Matata
+#Date : 18/02/2026
+# Group work
+
+Name = "Ernest Matata"
+Favorite_sports = "Football and basketball"
+Grade_attained = "B+"
+Favorite_hobby = "Doing research,listening to music and watching movies"
+favorite_course = "Computer Networks and Cybersecurity"
+favorite_food = "Chips,Nyama choma and pizza"
+
+print(f"My name is{Name}")
+print(f"My favorite sports are{Favorite_sports}")
+print(f"I attained the grade of {Grade_attained} in my KCSE")
+print(f"My favorite hobbies are{Favorite_hobby}")
+print(f"The course that I would want to pursue is{favorite_course}")
+print(f"The foods that I enjoy eating are{favorite_food}")
