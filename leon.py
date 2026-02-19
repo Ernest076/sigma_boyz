035dce1a914d6a4b9ec35a6d2f48dce96360cdf5
leon.lone <leonmuriithi.kobia@gmail.com>
2026-02-19 14:51:51 +0300

    leon.py

---
+++ leon.py
@@ -0,0 +1,10 @@
+#Leon Kobia
+#19/02/26
+name = "Leon Kobia"
+age = 18
+grade ="A-"
+course = "Aerospace Engineering"
+print(f"I am {name}")
+print(f"I am {age} years old")
+print(f"I scored an {grade}")
+print(f"I want to pursue {course}")
